5310a4c7a290b7bfcfb8b293676ae63343de889f
Roman Haefeli <reduzent@gmail.com>
2024-09-26 09:38:49 +0200

    add some flags to ensure correct rendering of pages

--- deploy.sh
+++ deploy.sh
@@ -2,5 +2,5 @@
 set -e errexit
 cd "$( dirname "${BASH_SOURCE[0]}" )"
 rm -rf public
-hugo
+hugo --gc --cleanDestinationDir --ignoreCache --disableFastRender
 rsync -av --delete public/. all@netpd.org:public_html/new/.
